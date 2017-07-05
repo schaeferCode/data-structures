6364303cfbcf9f2137aab865d143af4c66a990c8
Scott Schaefer <scottschaef@gmail.com>
2017-07-05 19:27:22 -0400

    Added push feature and counter

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return counter;
   };
 
   return someInstance;
