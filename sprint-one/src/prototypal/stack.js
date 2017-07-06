dc04f9b95f9ede11702689b8ef595bb757c57122
Scott Schaefer <scottschaef@gmail.com>
2017-07-06 19:17:31 -0400

    Created stack structure using prototypal style

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
 
+  pop: function() {
+    if (Object.keys(this.storage).length !== 0) {
+      this.counter--;
+      var result = this.storage[this.counter];
+      delete this.storage[this.counter];
+      return result;
+    }
+  },
 
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
