7e0b56d9b0504350a9ff0b606b1fe19f4c944cce
Scott Schaefer <scottschaef@gmail.com>
2017-07-06 10:19:06 -0400

    Added enqueue and dequeue functionality

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0;
+  var size = 0 + index;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    size = size - 1 + index;
+    if (size < 0) {
+      size = 0 + index;
+    }
+    return storage[size];
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
