fb1f4ec77246ce2f9c5681538d397e671f191643
Scott Schaefer <scottschaef@gmail.com>
2017-07-06 14:30:56 -0400

    Blasted out enqueue, dequeue, and size in record time

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = { storage: {}, indexToAdd: 0, indexToRemove: 0};
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // enqueue (value)
+  enqueue: function(value) {
+    this.storage[this.indexToAdd] = value;
+    this.indexToAdd++;
+  },
+  //   add value to this.storage
+  //   increase indexToAdd
+
+  // dequeue
+  dequeue: function() {
+    if (Object.keys(this.storage).length !== 0) {
+      var result = this.storage[this.indexToRemove];
+      delete this.storage[this.indexToRemove];
+      this.indexToRemove++;
+      return result;
+    }
+  },
+  //   if key length is not 0
+  //     save this.storage[this.indexToRemove] to value
+  //     delete this.storage[this.indexToRemove]
+  //     increase this.indexToRemove
+  //     return value
+
+  // size
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
