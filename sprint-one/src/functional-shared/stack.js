2f22beb742f5997285332785569b2c98ba2c918e
Scott Schaefer <scottschaef@gmail.com>
2017-07-06 14:09:24 -0400

    Added push functionality and size reporting
    Also created our storage structure.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,12 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    counter: 0
+  };
+  _.extend(someInstance, stackMethods);
 
-  // create function to add, remove and check size
+  return someInstance;
+};
+
+
+var stackMethods = {
+  // add push
+  push: function (value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+    // adding to storage
+    // increasing counter
+
+  // add pop
+    // decrease counter
+    // delete object
+
+    // return object being popped off the top
+
+  // add size
+  size: function() {
+    // return object.keys...
+    return Object.keys(this.storage).length;
+  }
 
 
 };
-
-var stackMethods = {};
-
-
