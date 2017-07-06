594074efd0ab829a6d84afb822465d120e2ccd42
Scott Schaefer <scottschaef@gmail.com>
2017-07-06 13:08:44 -0400

    Psuedocode

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create function to add, remove and check size
+
+
 };
 
 var stackMethods = {};
