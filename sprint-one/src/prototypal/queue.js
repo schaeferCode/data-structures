d40dd3e0a3fe2edfebe77da7dcb1658a95b252c0
Scott Schaefer <scottschaef@gmail.com>
2017-07-06 19:26:40 -0400

    Created queue structure using prototypal style

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.indexToAdd = 0;
+  someInstance.indexToRemove = 0;
+  return someInstance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function (value) {
+    this.storage[this.indexToAdd] = value;
+    this.indexToAdd++;
+  },
+
+  dequeue: function () {
+    if (Object.keys(this.storage).length !== 0) {
+      var result = this.storage[this.indexToRemove];
+      delete this.storage[this.indexToRemove];
+      this.indexToRemove++;
+      return result;
+    }
+  },
+
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+
+};
 
 
