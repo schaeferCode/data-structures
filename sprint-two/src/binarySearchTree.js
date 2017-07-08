3bae95508208da1eeb9384428077c3adc7fa855d
Scott Schaefer <scottschaef@gmail.com>
2017-07-08 19:55:24 -0400

    Made it look prettier

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,12 +1,15 @@
 var BinarySearchTree = function(value) {
+
   var storage = Object.create(BinarySearchTree.prototype);
   storage.value = value;
   storage.left = null;
   storage.right = null;
   return storage;
+
 };
 
 BinarySearchTree.prototype.insert = function (value) {
+
   if (value < this.value) {
 
     if (this.left) {
@@ -34,13 +37,12 @@ BinarySearchTree.prototype.insert = function (value) {
 
 BinarySearchTree.prototype.contains = function (target) {
 
-  //debugger;
 
   var isFound = false;
 
   function subContains (familyMember) {
+
     if (familyMember !== null && familyMember.value === target) {
-      // console.log(this.value, this.value === target);
       isFound = true;
     }
 
@@ -52,14 +54,14 @@ BinarySearchTree.prototype.contains = function (target) {
 
   }
 
-  // console.log(isFound);
   subContains(this);
+
   return isFound;
+
 };
 
 BinarySearchTree.prototype.depthFirstLog = function (callBack) {
-  // Base Case
-  // Run the callback function on the top of the tree/ at this
+
   callBack(this.value);
 
   if (this.left) {
@@ -69,22 +71,10 @@ BinarySearchTree.prototype.depthFirstLog = function (callBack) {
   if (this.right) {
     this.right.depthFirstLog(callBack);
   }
-  // Check if there is anything on this.left
-  //  Run depthFirstLog(this.left)
-  // Check if there is anything on this.right
-  //  Run depthFirstLog(this.right)
+
 };
+
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? Logarithmic
  */
 
-//var family = BinarySearchTree('scott');
-
-// family = {value: 5,
-//           left: BinarySearchTree(2) {
-//             value: '2',
-//             left: null,
-//             right: null
-//           },
-//           right: null
-//         }
