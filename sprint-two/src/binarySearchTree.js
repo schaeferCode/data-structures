ae233dda8b7bdc72c7c8ea9dd36277fcf08e3e2e
Scott Schaefer <scottschaef@gmail.com>
2017-07-08 18:47:20 -0400

    Create ability to add new binarySearchTree in the right place

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,55 @@
 var BinarySearchTree = function(value) {
+  var storage = Object.create(BinarySearchTree.prototype);
+  storage.value = value;
+  storage.left = null;
+  storage.right = null;
+  return storage;
 };
 
+BinarySearchTree.prototype.insert = function (value, familyMember) {
+  if (value < this.value) {
+    // if left is taken then go down the tree
+    if (this.left) {
 
+      this.left.insert(value)
+
+    } else if (!this.left) {
+
+      this.left = BinarySearchTree(value);
+
+    }
+  } else if (value > this.value) {
+
+    if (this.right) {
+
+      this.right.insert(value);
+
+    } else if (!this.right) {
+
+      this.right = BinarySearchTree(value);
+    }
+  }
+
+};
+
+BinarySearchTree.prototype.contains = function () {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function () {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//var family = BinarySearchTree('scott');
+
+// family = {value: 5,
+//           left: BinarySearchTree(2) {
+//             value: '2',
+//             left: null,
+//             right: null
+//           },
+//           right: null
+//         }
