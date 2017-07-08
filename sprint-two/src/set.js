b8a26a3221cc8037017e27bf73b4fdabdc9ca276
Scott Schaefer <scottschaef@gmail.com>
2017-07-08 13:38:14 -0400

    Created set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+
+  this._storage.push(item);
+
 };
 
 setPrototype.contains = function(item) {
+
+  return _.indexOf(this._storage, item) >= 0;
+
 };
 
 setPrototype.remove = function(item) {
+
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
+
 };
 
 /*
